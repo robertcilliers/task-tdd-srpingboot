92d336a57b7f3a72aea5a4a0bb6a4b103ec9c921
Robert <robertcilliers@gmail.com>
2023-08-23 22:34:12 +0200

    R

--- src/test/java/com/example/task/controller/TaskControllerTest.java
+++ src/test/java/com/example/task/controller/TaskControllerTest.java
@@ -19,6 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class TaskControllerTest {
 
+
     @Autowired
     private MockMvc mockMvc;
 
